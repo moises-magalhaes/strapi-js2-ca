82e8dcf2970694e1120160a8718fad288b8c42e0
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-03-03 11:13:23 +0100

    add to favorites
    add to favorites without localhost

--- js/favorites.js
+++ js/favorites.js
@@ -1,4 +1,4 @@
-import displayMessage from "./components/common/displayMessage.js";
+/*import displayMessage from "./components/common/displayMessage.js";
 import { baseUrl } from "./settings/api.js";
 import { saveToken, saveUser } from "./utils/storage.js";
 import createMenu from "./components/common/createMenu.js";
@@ -7,10 +7,25 @@ import createFooter from "./components/common/createFooter.js";
 createMenu();
 createFooter();
 
-let favorites = document.querySelectorAll(".add-to-fav");
+let addFavorites = document.getElementsByClassName("add-fav");
+console.log(addFavorites);
 
+addFavorites.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  console.log(event);
+  //this = event.target
+  event.target.classlist.toggle("fa");
+  event.target.classlist.toggle("far");
+}
+
+
+//const AddFavorites = document.querySelectorAll()
+/*
 for (let i = 0; i < favorites.length; i++) {
   favorites[i].addEventListener("click", () => {
     console.log("added to favorites");
   });
-}
+}*/
--- js/index.js
+++ js/index.js
@@ -11,23 +11,53 @@ createFooter();
 (async function () {
   const container = document.querySelector(".product-container");
 
+  //---------------------------------------------------//
+
+  let addFavorites = document.getElementsByClassName("far");
+  console.log(addFavorites); //
+
   try {
     const response = await fetch(articlesUrl);
     const json = await response.json();
 
     container.innerHTML = "";
-    // throw "custom message";
 
     json.forEach(function (article) {
       container.innerHTML += `<div class="article" href="/">
-                                        <h4> ${article.title}</h4>
-                                        <p> ${article.summary}</p>
-                                        <p>${article.author}</p>
-                                        <button class="add-to-fav">add to Favorites</button>    
-                                    </div>`;
+        <h4> ${article.title}</h4>
+        <p> ${article.summary}</p>
+        <p>${article.author}</p>
+        <span class="add-fav favorite">
+          add to Favorites: 
+          <i class="far fa-bookmark" data-id="${article.id}" data-name="${article.title}">
+          </i>
+        </span>    
+      </div>`;
     });
+
+    for (let i = 0; i < addFavorites.length; i++) {
+      const btn = addFavorites[i];
+
+      btn.addEventListener("click", function (event) {
+        console.log("clicked");
+        console.log(event);
+        event.target.classList.toggle("far");
+        event.target.classList.toggle("fas");
+
+        const id = this.dataset.id;
+        const name = this.dataset.name;
+
+        console.log("id", id);
+        console.log("name", name);
+      });
+    }
+
+    function getExistingFavs() {}
   } catch (error) {
     console.log(error);
     displayMessage("error", error, ".product-container");
   }
 })();
+
+//---------------------trying to edit  button ------------------//
+//--------------------- in the same page -------------------//
